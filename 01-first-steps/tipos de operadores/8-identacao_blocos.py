17e7f17880e66422c80b1d9b29a26c50995239a8
lucasjstn <lucas7justino@gmail.com>
2023-07-24 03:18:42 -0300

    add: identing and blocks

---
+++ 01-first-steps/tipos de operadores/8-identacao_blocos.py
@@ -0,0 +1,12 @@
+saldo = 199
+
+def sacar(valor):
+    if saldo >= valor:
+        print('valor sacado')
+    print('isso será exibido de qualquer forma fora do if')
+    
+def depositar(valor):
+saldo = 500
+saldo <= valor
+
+sacar(300)
